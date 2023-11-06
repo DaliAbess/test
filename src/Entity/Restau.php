b4b3dfbddd0d0012a4d319f74c273f366f859464
daliabess7 <daliabess7@gmail.com>
2023-11-06 22:54:02 +0000

    first version

---
+++ src/Entity/Restau.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RestauRepository::class)
+ */
+class Restau
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $log;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLog(): ?string
+    {
+        return $this->log;
+    }
+
+    public function setLog(string $log): self
+    {
+        $this->log = $log;
+
+        return $this;
+    }
+
+    public function getLat(): ?string
+    {
+        return $this->lat;
+    }
+
+    public function setLat(string $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+}
